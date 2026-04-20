7e6da483ec31ac01c3471167369e6c37e4f01c36
GiovaniBenvenutti <giovanibenvenutti2010@hotmail.com>
2026-04-20 06:13:32 -0300

    teste

--- Assets/_Scripts/NewBehaviourScript.cs
+++ Assets/_Scripts/NewBehaviourScript.cs
@@ -12,7 +12,7 @@ public class NewBehaviourScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("Hello World");
     }
 
     // Update is called once per frame
