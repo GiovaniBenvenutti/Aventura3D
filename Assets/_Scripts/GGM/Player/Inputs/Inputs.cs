701ae6ae3425b3e8920dc917cd3cbb9e8fa68221
Giovani <giovanibenvenutti2010@hotmail.com>
2026-05-23 10:44:53 -0300

    implementei a troca de armas usando as teclas 1,2, 3

--- Assets/_Scripts/GGM/Player/Inputs/Inputs.cs
+++ Assets/_Scripts/GGM/Player/Inputs/Inputs.cs
@@ -1,4 +1,4 @@
-// GENERATED AUTOMATICALLY FROM 'Assets/_Scripts/GGM/Player/Inputs.inputactions'
+// GENERATED AUTOMATICALLY FROM 'Assets/_Scripts/GGM/Player/Inputs/Inputs.inputactions'
 
 using System;
 using System.Collections;
@@ -25,6 +25,30 @@ public class @Inputs : IInputActionCollection, IDisposable
                     ""expectedControlType"": ""Button"",
                     ""processors"": """",
                     ""interactions"": """"
+                },
+                {
+                    ""name"": ""changeGun1"",
+                    ""type"": ""Button"",
+                    ""id"": ""a1949eb9-d32f-4928-9726-24ce3cedfc69"",
+                    ""expectedControlType"": ""Button"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                },
+                {
+                    ""name"": ""changeGun2"",
+                    ""type"": ""Button"",
+                    ""id"": ""503481fc-463b-4aaf-b1f5-237b0b787c3b"",
+                    ""expectedControlType"": ""Button"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                },
+                {
+                    ""name"": ""changeGun3"",
+                    ""type"": ""Button"",
+                    ""id"": ""d11f89a8-7ba2-4040-8d69-47288e1d0dcd"",
+                    ""expectedControlType"": ""Button"",
+                    ""processors"": """",
+                    ""interactions"": """"
                 }
             ],
             ""bindings"": [
@@ -38,6 +62,39 @@ public class @Inputs : IInputActionCollection, IDisposable
                     ""action"": ""shoot"",
                     ""isComposite"": false,
                     ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""8314477e-dfdf-45a0-a2a9-c11f04ca3c76"",
+                    ""path"": ""<Keyboard>/1"",
+                    ""interactions"": ""Press"",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""changeGun1"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""bab7d4ca-bed8-497c-a340-b9aeef5ccae3"",
+                    ""path"": ""<Keyboard>/2"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""changeGun2"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""7d9e7089-0130-4fab-91c9-502be3bfcfad"",
+                    ""path"": ""<Keyboard>/3"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""changeGun3"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
                 }
             ]
         }
@@ -47,6 +104,9 @@ public class @Inputs : IInputActionCollection, IDisposable
         // GamePlay
         m_GamePlay = asset.FindActionMap("GamePlay", throwIfNotFound: true);
         m_GamePlay_shoot = m_GamePlay.FindAction("shoot", throwIfNotFound: true);
+        m_GamePlay_changeGun1 = m_GamePlay.FindAction("changeGun1", throwIfNotFound: true);
+        m_GamePlay_changeGun2 = m_GamePlay.FindAction("changeGun2", throwIfNotFound: true);
+        m_GamePlay_changeGun3 = m_GamePlay.FindAction("changeGun3", throwIfNotFound: true);
     }
 
     public void Dispose()
@@ -97,11 +157,17 @@ public class @Inputs : IInputActionCollection, IDisposable
     private readonly InputActionMap m_GamePlay;
     private IGamePlayActions m_GamePlayActionsCallbackInterface;
     private readonly InputAction m_GamePlay_shoot;
+    private readonly InputAction m_GamePlay_changeGun1;
+    private readonly InputAction m_GamePlay_changeGun2;
+    private readonly InputAction m_GamePlay_changeGun3;
     public struct GamePlayActions
     {
         private @Inputs m_Wrapper;
         public GamePlayActions(@Inputs wrapper) { m_Wrapper = wrapper; }
         public InputAction @shoot => m_Wrapper.m_GamePlay_shoot;
+        public InputAction @changeGun1 => m_Wrapper.m_GamePlay_changeGun1;
+        public InputAction @changeGun2 => m_Wrapper.m_GamePlay_changeGun2;
+        public InputAction @changeGun3 => m_Wrapper.m_GamePlay_changeGun3;
         public InputActionMap Get() { return m_Wrapper.m_GamePlay; }
         public void Enable() { Get().Enable(); }
         public void Disable() { Get().Disable(); }
@@ -114,6 +180,15 @@ public class @Inputs : IInputActionCollection, IDisposable
                 @shoot.started -= m_Wrapper.m_GamePlayActionsCallbackInterface.OnShoot;
                 @shoot.performed -= m_Wrapper.m_GamePlayActionsCallbackInterface.OnShoot;
                 @shoot.canceled -= m_Wrapper.m_GamePlayActionsCallbackInterface.OnShoot;
+                @changeGun1.started -= m_Wrapper.m_GamePlayActionsCallbackInterface.OnChangeGun1;
+                @changeGun1.performed -= m_Wrapper.m_GamePlayActionsCallbackInterface.OnChangeGun1;
+                @changeGun1.canceled -= m_Wrapper.m_GamePlayActionsCallbackInterface.OnChangeGun1;
+                @changeGun2.started -= m_Wrapper.m_GamePlayActionsCallbackInterface.OnChangeGun2;
+                @changeGun2.performed -= m_Wrapper.m_GamePlayActionsCallbackInterface.OnChangeGun2;
+                @changeGun2.canceled -= m_Wrapper.m_GamePlayActionsCallbackInterface.OnChangeGun2;
+                @changeGun3.started -= m_Wrapper.m_GamePlayActionsCallbackInterface.OnChangeGun3;
+                @changeGun3.performed -= m_Wrapper.m_GamePlayActionsCallbackInterface.OnChangeGun3;
+                @changeGun3.canceled -= m_Wrapper.m_GamePlayActionsCallbackInterface.OnChangeGun3;
             }
             m_Wrapper.m_GamePlayActionsCallbackInterface = instance;
             if (instance != null)
@@ -121,6 +196,15 @@ public class @Inputs : IInputActionCollection, IDisposable
                 @shoot.started += instance.OnShoot;
                 @shoot.performed += instance.OnShoot;
                 @shoot.canceled += instance.OnShoot;
+                @changeGun1.started += instance.OnChangeGun1;
+                @changeGun1.performed += instance.OnChangeGun1;
+                @changeGun1.canceled += instance.OnChangeGun1;
+                @changeGun2.started += instance.OnChangeGun2;
+                @changeGun2.performed += instance.OnChangeGun2;
+                @changeGun2.canceled += instance.OnChangeGun2;
+                @changeGun3.started += instance.OnChangeGun3;
+                @changeGun3.performed += instance.OnChangeGun3;
+                @changeGun3.canceled += instance.OnChangeGun3;
             }
         }
     }
@@ -128,5 +212,8 @@ public class @Inputs : IInputActionCollection, IDisposable
     public interface IGamePlayActions
     {
         void OnShoot(InputAction.CallbackContext context);
+        void OnChangeGun1(InputAction.CallbackContext context);
+        void OnChangeGun2(InputAction.CallbackContext context);
+        void OnChangeGun3(InputAction.CallbackContext context);
     }
 }
--- Assets/_Scripts/GGM/Player/Player_Abilities/PlayerAbilityShoot.cs
+++ Assets/_Scripts/GGM/Player/Player_Abilities/PlayerAbilityShoot.cs
@@ -5,7 +5,7 @@ using UnityEngine.InputSystem;
 
 public class PlayerAbilityShoot : PlayerAbilityBase
 {
-    public GunBase3D gunBase;
+    public List<GunBase3D> gunBase;
     public Transform gunPosition;
     private GunBase3D _currentGun;
 
@@ -15,13 +15,18 @@ public class PlayerAbilityShoot : PlayerAbilityBase
         createGun();
         inputs.GamePlay.shoot.performed += ctx => StartShoot();
         inputs.GamePlay.shoot.canceled += ctx => CancelShoot();
+        inputs.GamePlay.changeGun1.performed += ctx => ChangeGun(0);
+        inputs.GamePlay.changeGun2.performed += ctx => ChangeGun(1);
+        inputs.GamePlay.changeGun3.performed += ctx => ChangeGun(2);
+
     }
+
 
     private void createGun()
     {
         if (_currentGun != null) Destroy(_currentGun.gameObject);
 
-        _currentGun = Instantiate(gunBase, gunPosition);
+        _currentGun = Instantiate(gunBase[0], gunPosition);
         _currentGun.transform.localPosition = _currentGun.transform.localEulerAngles = Vector3.zero;
         
     }
@@ -42,5 +47,21 @@ public class PlayerAbilityShoot : PlayerAbilityBase
         // Implement shooting logic here
         Debug.Log("Player cancels shoot!");
         _currentGun.StopShoot();
+    }
+
+    public void ChangeGun(int index)
+    {
+        if (index < 0 || index >= gunBase.Count)
+        {
+            Debug.LogWarning("Invalid gun index!");
+            return;
+        }
+
+        if (_currentGun != null && _currentGun != gunBase[index])
+        {
+            _currentGun.StopShoot();
+            _currentGun = Instantiate(gunBase[index], gunPosition);
+            _currentGun.transform.localPosition = _currentGun.transform.localEulerAngles = Vector3.zero;
+        }
     }
 }
