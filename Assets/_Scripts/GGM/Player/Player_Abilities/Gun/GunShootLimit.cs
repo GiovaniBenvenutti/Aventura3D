fdf70e41dbf6ff12ea2c2a901209a3d34802c802
Giovani <giovanibenvenutti2010@hotmail.com>
2026-05-23 10:16:03 -0300

    added gun ui

--- Assets/_Scripts/GGM/Player/Player_Abilities/Gun/GunShootLimit.cs
+++ Assets/_Scripts/GGM/Player/Player_Abilities/Gun/GunShootLimit.cs
@@ -1,15 +1,22 @@
+using System.Linq;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class GunShootLimit : GunBase3D
 {
+    public List<UiGunUpdater> uiGunUpdaters;
     public int maxShoot = 5;
     public float timeToRecharge = 1f;
 
     private float _currentShoots;
     private bool _isRecharging;
 
+    private void Awake()
+    {
+        GetAllUis();
+        UpdateUi();
+    }
 
     protected override IEnumerator ShootCoroutine()
     {
@@ -21,6 +28,7 @@ public class GunShootLimit : GunBase3D
                 Shoot();
                 _currentShoots++;
                 checkRecharge();
+                UpdateUi();
                 yield return new WaitForSeconds(timeBetweenShoot);
             }
         }
@@ -47,10 +55,27 @@ public class GunShootLimit : GunBase3D
         while(timer < timeToRecharge)
         {
             timer += Time.deltaTime;
+            uiGunUpdaters.ForEach(ui => ui.uiUpdateValue(timer / timeToRecharge));
+
             yield return new WaitForEndOfFrame();   // usar um waitforseconds aqui não é recomendado, pois se o timeToRecharge for menor que o tempo de um frame, o timer nunca vai chegar no valor necessário para recarregar
         }   
         _currentShoots = 0;
         _isRecharging = false;
+    }
+
+    private void UpdateUi()
+    {
+        uiGunUpdaters.ForEach(ui => ui.uiUpdateValue(maxShoot, _currentShoots));
+        
+        // foreach(var ui in uiGunUpdaters)
+        // {
+        //     ui.uiUpdateValue(maxShoot, maxShoot - _currentShoots);
+        // }
+    }
+
+    private void GetAllUis()
+    {
+        uiGunUpdaters = FindObjectsOfType<UiGunUpdater>().ToList();
     }
 
 }
---
+++ Assets/_Scripts/GGM/Player/Player_Abilities/Gun/UiGunUpdater.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+
+public class UiGunUpdater : MonoBehaviour
+{
+    public Image UiImage;
+
+    [Header("Animation")]
+    public float duration = 0.1f;
+    public Ease ease = Ease.OutBack;
+
+    private Tween _currentTween;
+
+
+    private void OnValidate()
+    {
+        if (UiImage == null) UiImage = GetComponent<Image>();    
+    }
+
+    public void uiUpdateValue(float f)
+    {
+        UiImage.fillAmount = f;
+    }
+
+    public void uiUpdateValue(float max, float currente)
+    {
+        // _currentTween?.Kill();
+        if(_currentTween != null) _currentTween.Kill();
+        // UiImage.fillAmount = 1 - (currente / max);
+        _currentTween = UiImage.DOFillAmount(1 - (currente / max), duration).SetEase(ease);
+    }
+}
